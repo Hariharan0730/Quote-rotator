c33f44213ba5b247a6440b13d7a4b16353fd908b
Hariharan0730 <haranhari0730@gmail.com>
2024-10-23 21:07:50 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+import {React, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const quotes = [
+"A full-stack developer is the Swiss Army knife of coding, equipped to tackle both the front-end user experience and the back-end logic with equal proficiency.",
+"Being a full-stack developer means being a bridge between the user's needs and the server's capabilities, making the digital world a seamless experience.",
+"A full-stack developer doesn't just build apps—they engineer solutions from the database to the interface, crafting end-to-end functionality.",
+"To be a full-stack developer is to understand the complete lifecycle of an application, from concept to deployment, and everything in between."
+];
 
-export default App;
+function QuotesRotator(){
+  const [index,setIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() =>{
+      setIndex((index + 1) % quotes.length);
+    }, 2000);
+    return () => clearInterval(interval);  
+    },[index]);
+    return(
+      <div>
+        <h2>Quotes Rotators</h2>
+        <p>{quotes[index]}</p>
+      </div>
+    )
+}
+export default QuotesRotator;
